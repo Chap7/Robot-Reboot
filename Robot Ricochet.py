7dbcdc375ec1a7f6cbb57beb1991fed846c0f2a2
Alex Khau <prenom.nom@ens.uvsq.fr>
2021-05-23 17:24:57 +0200

    Ajout image

--- Robot Ricochet.py
+++ Robot Ricochet.py
@@ -1,6 +1,6 @@
 import random
 import tkinter as tk
-
+from tkinter import PhotoImage
 root = tk.Tk()
 
 """ 
@@ -369,6 +369,14 @@ def handle_click(event): #ok
 # Bind une touche event pour handle_keypress()
 root.bind("<Key>", handle_keypress)
 root.bind("<Button-1>", handle_click)
+
+
+
+canvas = tk.Canvas(bg = "white", width = LARGEUR, height = HAUTEUR)
+img = PhotoImage(file='restart.ppm')
+canvas.create_image(HAUTEUR/2, LARGEUR/2, image=img)
+
+
 
 canvas.create_rectangle(850, 250, 1470, 600, width = 15)
 tableau_coord = placer_case(tableau)
